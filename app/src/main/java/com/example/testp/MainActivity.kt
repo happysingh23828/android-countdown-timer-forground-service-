cafdd1bdacc2639d8b3ec53d66f493fdbabe9188
happysingh23828 <happysingh23828@gmail.com>
2022-04-13 19:03:12 +0530

    implemented countdown timer with forground service

--- app/src/main/java/com/example/testp/MainActivity.kt
+++ app/src/main/java/com/example/testp/MainActivity.kt
@@ -3,15 +3,81 @@ package com.example.testp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.content.Intent
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.example.testp.services.CountdownTimerService
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 class MainActivity : AppCompatActivity() {
 
     companion object {
         const val INTENT_COUNTDOWN_MINUTES = "intent_countdown_minutes"
     }
 
+    lateinit var timerBrodReceiver: TimerStatusReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        onClicks()
     }
+
+    private fun init() {
+        timerBrodReceiver = TimerStatusReceiver()
+    }
+
+    private fun onClicks() {
+        btnTimer.setOnClickListener {
+            if (isValidInput()) {
+                val minutes = edtTime.text.toString().toLong()
+                startTimer(minutes)
+            } else {
+                Toast.makeText(this, "Enter minutes", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnTimerStop.setOnClickListener {
+            stopTimer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(this)
+            .registerReceiver(timerBrodReceiver, IntentFilter(CountdownTimerService.TIME_INFO));
+    }
+
+    private fun startTimer(minutes: Long) {
+        val intent = Intent(this, CountdownTimerService::class.java)
+        intent.putExtra(INTENT_COUNTDOWN_MINUTES, minutes)
+        startService(intent)
+    }
+
+    private fun stopTimer() {
+        val intent = Intent(this, CountdownTimerService::class.java)
+        stopService(intent)
+    }
+
+    private fun isValidInput(): Boolean {
+        return edtTime.text.toString().isNotEmpty();
+    }
+
+
+    inner class TimerStatusReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent != null && intent.action == CountdownTimerService.TIME_INFO) {
+                if (intent.hasExtra("VALUE")) {
+                    tvCountdownTimer.text = intent.getStringExtra("VALUE")
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/testp/services/CountdownTimerService.kt
+++ app/src/main/java/com/example/testp/services/CountdownTimerService.kt
@@ -1,28 +1,34 @@
 package com.example.testp.services
 
 import android.annotation.SuppressLint
-import android.app.Notification
+import android.app.*
+import android.content.Context
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 import android.content.Intent
+import android.graphics.Color
 
 import android.os.CountDownTimer
 
 
 import androidx.core.app.NotificationCompat
 
-import android.app.PendingIntent
-import android.app.Service
+import android.os.Build
 
 import android.os.IBinder
 import androidx.annotation.Nullable
+import androidx.annotation.RequiresApi
 import com.example.testp.MainActivity
 import com.example.testp.MainActivity.Companion.INTENT_COUNTDOWN_MINUTES
 import com.example.testp.R
 import java.util.concurrent.TimeUnit
+import android.app.NotificationManager
+import android.app.PendingIntent
 
 
 class CountdownTimerService : Service() {
+
+
     private var timer: CounterClass? = null
 
 
@@ -34,70 +40,117 @@ class CountdownTimerService : Service() {
     @SuppressLint("WrongConstant")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        val totalMinutes = intent?.getLongExtra(INTENT_COUNTDOWN_MINUTES,-1)
+        val totalMinutes = intent?.getLongExtra(INTENT_COUNTDOWN_MINUTES, -1)
+
         if (totalMinutes != null && totalMinutes != -1L) {
 
             val totalInMillis = totalMinutes * 60 * 1000
-            timer = CounterClass(totalInMillis , 1000)
+            timer = CounterClass(totalInMillis, 1000)
             timer!!.start()
 
-            val notificationIntent = Intent(this, MainActivity::class.java)
-            val pendingIntent = PendingIntent.getActivity(
-                this, 0,
-                notificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK
-            )
-            val notification: Notification = NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentText("Timer is Running")
-                .setContentIntent(pendingIntent).build()
-            startForeground(101, notification)
+            val notification = getNotification(getFormattedTime(totalInMillis));
+
+            startForeground(NOTIFICATION_ID, notification)
         }
 
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
-        timer!!.cancel()
+        timer?.cancel()
         super.onDestroy()
-        val timerInfoIntent = Intent(TIME_INFO)
-        timerInfoIntent.putExtra("VALUE", "Stopped")
-        LocalBroadcastManager.getInstance(this@CountdownTimerService).sendBroadcast(timerInfoIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(
+            channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE
+        )
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+
+    @SuppressLint("WrongConstant")
+    private fun getNotification(timerText: String): Notification {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.getActivity(
+                this, 0,
+                notificationIntent, PendingIntent.FLAG_IMMUTABLE
+            )
+        } else {
+            PendingIntent.getActivity(
+                this, 0,
+                notificationIntent, Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+        }
+        val channelId =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel("my_service", "My Background Service")
+            } else {
+                ""
+            }
+
+        return NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentText(timerText)
+            .setContentIntent(pendingIntent).build()
+    }
+
+    /**
+     * This is the method that can be called to update the Notification
+     */
+    private fun updateTimerNotification(timerText: String) {
+        val notification: Notification = getNotification(timerText)
+        val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        mNotificationManager.notify(NOTIFICATION_ID, notification)
     }
 
     inner class CounterClass(millisInFuture: Long, countDownInterval: Long) :
         CountDownTimer(millisInFuture, countDownInterval) {
         override fun onTick(millisUntilFinished: Long) {
-            val hms = String.format(
-                "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(
-                    millisUntilFinished
-                ),
-                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
-                    TimeUnit.MILLISECONDS.toHours(
-                        millisUntilFinished
-                    )
-                ),
-                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
-                    TimeUnit.MILLISECONDS.toMinutes(
-                        millisUntilFinished
-                    )
-                )
-            )
+            val hms = getFormattedTime(millisUntilFinished)
             println(hms)
             val timerInfoIntent = Intent(TIME_INFO)
             timerInfoIntent.putExtra("VALUE", hms)
+
+            updateTimerNotification(hms)
             LocalBroadcastManager.getInstance(this@CountdownTimerService)
                 .sendBroadcast(timerInfoIntent)
         }
 
         override fun onFinish() {
-            val timerInfoIntent = Intent(TIME_INFO)
-            timerInfoIntent.putExtra("VALUE", "Completed")
-            LocalBroadcastManager.getInstance(this@CountdownTimerService)
-                .sendBroadcast(timerInfoIntent)
+            // do nothing
         }
     }
 
+
+    private fun getFormattedTime(millisUntilFinished: Long): String {
+        val hms = String.format(
+            "%02d:%02d:%02d", TimeUnit.MILLISECONDS.toHours(
+                millisUntilFinished
+            ),
+            TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                TimeUnit.MILLISECONDS.toHours(
+                    millisUntilFinished
+                )
+            ),
+            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                TimeUnit.MILLISECONDS.toMinutes(
+                    millisUntilFinished
+                )
+            )
+        )
+        return "Current timer $hms"
+    }
+
     companion object {
+        const val NOTIFICATION_ID = 101
         const val TIME_INFO = "time_info"
     }
 }
